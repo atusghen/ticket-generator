2749659fa0f02bf9896af73a35bb1718197152ba
Salvatore Greco <grecosalvatore66662@gmail.com>
2022-12-10 12:47:52 +0100

    some modify

--- ticketgeneratorServer/src/main/java/com/unibg/ticketgenerator/entities/Utente.java
+++ ticketgeneratorServer/src/main/java/com/unibg/ticketgenerator/entities/Utente.java
@@ -70,6 +70,26 @@ public class Utente {
         this.password = password;
     }
 
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String nome;
     private String cognome;
     private String cf;
